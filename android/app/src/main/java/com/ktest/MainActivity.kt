126bbc9a57cee0dc0bc11f77c87daaca90e157a4
decrua <baturin@tuta.io>
2025-08-01 16:41:50 +0700

    9

--- android/app/src/main/java/com/ktest/MainActivity.kt
+++ android/app/src/main/java/com/ktest/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.ktest
 
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle // <-- Добавьте этот импорт
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-// Возвращаем к стандартному виду
 class MainActivity : ReactActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState) // <-- Обязательно вызовите super.onCreate
+    
+    // ЗАПУСКАЕМ СЛУЖБУ ЗДЕСЬ
+    val serviceIntent = Intent(this, MediaKeyService::class.java)
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        startForegroundService(serviceIntent)
+    } else {
+        startService(serviceIntent)
+    }
+  }
 
   override fun getMainComponentName(): String = "ktest"
 
--- android/app/src/main/java/com/ktest/MainApplication.kt
+++ android/app/src/main/java/com/ktest/MainApplication.kt
@@ -1,15 +1,12 @@
 package com.ktest
 
 import android.app.Application
-import android.content.Intent
-import android.os.Build
-import android.widget.Toast
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load // <-- Correct import
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
@@ -24,11 +21,8 @@ class MainApplication : Application(), ReactApplication {
             }
 
         override fun getJSMainModuleName(): String = "index"
-
         override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
       }
 
@@ -37,25 +31,10 @@ class MainApplication : Application(), ReactApplication {
 
   override fun onCreate() {
     super.onCreate()
-    Toast.makeText(this, "2: Приложение запущено", Toast.LENGTH_SHORT).show()
     SoLoader.init(this, false)
-
-    // CORRECTED BLOCK FOR NEW ARCHITECTURE
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    
-    startMediaKeyService()
-  }
-
-  private fun startMediaKeyService() {
-      val serviceIntent = Intent(this, MediaKeyService::class.java)
-      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-          startForegroundService(serviceIntent)
-      } else {
-          startService(serviceIntent)
-      }
+    // ЗАПУСК СЛУЖБЫ ОТСЮДА УБРАН!
   }
 }
-
